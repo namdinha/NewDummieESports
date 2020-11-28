4258a12570d26775a6ea653c3d21525f8d821758
thewhitesummer <fe_granger@hotmail.com>
2020-11-28 17:36:05 -0300

    initial commit

---
+++ Assets/Scripts/Legs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legs : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    ConstantForce force;
+    Vector3 inicialPos;
+
+    private void Awake() {
+        this.rigidbody = GetComponent<Rigidbody>();
+        this.force = GetComponent<ConstantForce>();
+        inicialPos = this.rigidbody.position;
+    }
+
+    void Update()
+    {
+        Vector3 pos = inicialPos - this.rigidbody.position;
+        ////pos.x = 0;
+        ////pos.z = 0;
+        //force.force = pos * 500;
+        //Debug.Log(rigidbody.position);
+    }
+}
---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveTest : MonoBehaviour
+{
+    private CharacterJoint[] joints;
+
+    [SerializeField]
+    private Rigidbody head;
+    [SerializeField]
+    private Rigidbody spine;
+    [SerializeField]
+    private Rigidbody hips;
+    [SerializeField]
+    private Rigidbody leftLeg;
+    [SerializeField]
+    private Rigidbody rightLeg;
+    [SerializeField]
+    private Rigidbody leftKnee;
+    [SerializeField]
+    private Rigidbody rightKnee;
+
+    private void Awake() {
+        //joints = GetComponentsInChildren<CharacterJoint>();
+        //foreach (CharacterJoint joint in joints) {
+        //    joint.connectedmassscale = 1;
+        //}
+        //leftKnee.mass += 20;
+        //rightKnee.mass += 20;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            //foreach (CharacterJoint joint in joints) {
+            //    joint.connectedMassScale += 10;
+            //}
+            leftKnee.AddRelativeForce(new Vector3(0, 1, 0));
+        }
+        else if (Input.GetButtonDown("Fire2")) {
+            //foreach (CharacterJoint joint in joints) {
+            //    joint.connectedMassScale -= 10;
+            //}
+        }
+
+        
+    }
+}
